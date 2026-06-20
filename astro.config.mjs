99173b6001af2f19a025f580ddaedd360783bafd
Paolo Minhas <paolo.minhas@gmail.com>
2026-06-20 18:26:47 +0100

    vercel compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://dioramaconsulting.co.uk',
-	integrations: [mdx(), sitemap()],
+    site: 'https://dioramaconsulting.co.uk',
+    integrations: [mdx(), sitemap()],
     output: 'static',
-    adapter: node({ mode: 'standalone' }),
+    adapter: vercel(),
 });
